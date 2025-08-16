77d6e0bf162cc2ec85c81fd173047f495e6dbc9e
chloeduw <120414368+chloeduw@users.noreply.github.com>
2025-08-16 23:57:59 +0200

    Delete Degradation Detection/+EHMCell directory

--- Degradation Detection/+EHMCell/conditionalDensityFct.m
+++
@@ -1,6 +0,0 @@
-function f = conditionalDensityFct(NT,innov,S)
-    f = zeros(1,NT);
-    for i=1:NT
-        f(i) = 1/((2*pi)^(1/2)*sqrt(det(S(i))))*exp(-1/2*innov(i)'*(S(i)\innov(i)));
-    end
-end
--- Degradation Detection/+EHMCell/deta.m
+++
@@ -1,5 +0,0 @@
-function deta = deta(a,b,c,x) %derivative of the electrode overpotentials
-    num = -a*b*c*(1-2*x);
-    denom = 2*c.*x.*(1-x).*sqrt(c^2.*x.*(1-x)+b^2);
-    deta = num./denom;
-end
